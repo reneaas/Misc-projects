2caaa0a9b8f0895699d45274c76ae311d97f36aa
reneaas <reneaas@student.matnat.uio.no>
2020-07-25 19:12:46 +0200

    update

--- testing/Matrixdotnorm/test.py
+++ testing/Matrixdotnorm/test.py
@@ -36,9 +36,3 @@ F = np.fft.fft(func_vals)
 end_time = time.time()
 print("FFT: Took {} seconds".format(end_time-start_time))
 
-
-"""
-ubuntu 20.04
-https://browser.geekbench.com/v5/cpu/3029256/claim?key=131284
-
-"""
