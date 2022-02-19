69c9c59bee11e5dc061b7e8f962c8e66b1331f5f
reneaas <reneaas@uio.no>
2022-02-19 18:00:11 +0100

    Cleaned up main program

--- solar_system/cython/main.py
+++ solar_system/cython/main.py
@@ -14,36 +14,15 @@ def main():
 
     num_particles = r0.shape[0]
 
-    system = PySolarSystem(r0=r0, v0=v0, m=m)
-    pos = system.get_position()
-    vel = system.get_velocity()
-    print(f"{pos=}")
-    print(f"{vel=}")
-
-    num_objects = system.num_objects
-    print(f"{num_objects=}")
-
-    mass = system.m
-    print(f"{mass=}")
-
-    force = np.asarray(system.get_force())
-    print(f"{force=}")
-
-    system.step(force=force, dt=0.001)
-
-    pos = system.get_position()
-    vel = system.get_velocity()
-    print(f"{pos=}")
-    print(f"{vel=}")
-
+    solar_system = PySolarSystem(r0=r0, v0=v0, m=m)
     num_iter = 1000000
     r = np.zeros(shape=(num_iter, num_particles, 3))
     v = np.zeros(shape=(num_iter, num_particles, 3))
-    for i in trange(num_iter):
-        force = system.get_force()
-        system.step(force=force, dt=0.001)
-        new_pos = system.get_position()
-        new_vel = system.get_velocity()
+    for i in trange(num_iter, desc="Calculating orbits"):
+        force = solar_system.get_force()
+        solar_system.step(force=force, dt=0.001)
+        new_pos = solar_system.get_position()
+        new_vel = solar_system.get_velocity()
         r[i, ...] = new_pos
         v[i, ...] = new_vel
     
